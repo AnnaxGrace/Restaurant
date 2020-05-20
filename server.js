16cb0242c4efa021eb004de6881b30f7fa381507
AnnaGrace <anna.grace.conover@gmail.com>
2020-05-20 15:46:54 -0500

    Set up reservation.html and server.js

---
+++ server.js
@@ -0,0 +1,28 @@
+var express = require("express");
+var path = require("path");
+
+var app = express();
+var PORT = 8080;
+
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get("/reservation", function(req,res) {
+    res.sendFile(path.join(__dirname, "reservation.html"));
+});
+
+app.get("/tables", function(req,res) {
+    res.sendFile(path.join(__dirname, "tables.html"));
+});
+
+app.get("api/tables", function(req, res) {
+   return res.json(tables);
+});
+
+app.get("api/waitlist", function(req, res) {
+    return res.json(waitlist);
+});
